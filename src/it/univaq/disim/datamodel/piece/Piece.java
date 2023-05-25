c424667082c75a834bdf58bbd926db932721d93b
fcapricci <capricci.federico@gmail.com>
2023-05-25 16:13:58 +0200

    classe pezzo

---
+++ src/it/univaq/disim/datamodel/piece/Piece.java
@@ -0,0 +1,13 @@
+package src.it.univaq.disim.datamodel.piece;
+
+import lombok.Data;
+
+@Data public abstract class Piece {
+	
+	protected int xCord;
+	
+	protected int yCord;
+	
+	protected String color;
+
+}
