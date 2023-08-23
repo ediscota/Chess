c64ce8f686d6c6975527d518bfc1da3be43289a4
fcapricci <capricci.federico@gmail.com>
2023-08-23 15:02:34 +0200

    partite ordinate in base al numero di pezzi in campo

--- src/main/java/it/univaq/disim/service/Statistic.java
+++ src/main/java/it/univaq/disim/service/Statistic.java
@@ -42,6 +42,10 @@ public class Statistic implements Serializable {
 			mergeSort(deserializeGames, 0, deserializeGames.length - 1);
 
 	        System.out.println("Partite ordinate in base alle mosse: " + Arrays.toString(deserializeGames));
+	        
+	        mergeSort1(deserializeGames, 0, deserializeGames.length - 1);
+	        
+	        System.out.println("Partite ordinate in base alle mosse: " + Arrays.toString(deserializeGames));
 
 		}
 	}
@@ -55,6 +59,7 @@ public class Statistic implements Serializable {
 	            mergeSort(deserializeGames, middle + 1, right);
 
 	            merge(deserializeGames, left, middle, right);
+	            
 	        }
 	    }
 
@@ -76,6 +81,57 @@ public class Statistic implements Serializable {
 
 	        while (i < n1 && j < n2) {
 	            if (leftArray[i].getBlackMoves().size()+leftArray[i].getWhiteMoves().size() >= rightArray[j].getBlackMoves().size()+ rightArray[j].getWhiteMoves().size()) {
+	            	deserializeGames[k] = leftArray[i];
+	                i++;
+	            } else {
+	            	deserializeGames[k] = rightArray[j];
+	                j++;
+	            }
+	            k++;
+	        }
+
+	        while (i < n1) {
+	        	deserializeGames[k] = leftArray[i];
+	            i++;
+	            k++;
+	        }
+
+	        while (j < n2) {
+	        	deserializeGames[k] = rightArray[j];
+	            j++;
+	            k++;
+	        }
+	    }
+	    
+	    public static void mergeSort1(Game[] deserializeGames, int left, int right) {
+	        if (left < right) {
+	            int middle = (left + right) / 2;
+
+	            mergeSort(deserializeGames, left, middle);
+	            mergeSort(deserializeGames, middle + 1, right);
+
+	            merge1(deserializeGames, left, middle, right);
+	        }
+	    }
+
+	    public static void merge1(Game[] deserializeGames, int left, int middle, int right) {
+	        int n1 = middle - left + 1;
+	        int n2 = right - middle;
+
+	        Game[] leftArray = new Game[n1];
+	        Game[] rightArray = new Game[n2];
+
+	        for (int i = 0; i < n1; i++) {
+	            leftArray[i] = deserializeGames[left + i];
+	        }
+	        for (int j = 0; j < n2; j++) {
+	            rightArray[j] = deserializeGames[middle + 1 + j];
+	        }
+
+	        int i = 0, j = 0, k = left;
+
+	        while (i < n1 && j < n2) {
+	            if (leftArray[i].getDeadBlackPieces().size()+leftArray[i].getDeadWhitePieces().size() < rightArray[j].getDeadBlackPieces().size()+ rightArray[j].getDeadWhitePieces().size()) {
 	            	deserializeGames[k] = leftArray[i];
 	                i++;
 	            } else {
